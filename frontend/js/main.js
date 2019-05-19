f2bb386fd9057d5a80c215c36640d84549bacd82
luis572 <luispg572@gmail.com>
2019-05-19 08:56:58 -0500

    correccion

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,32 +1,57 @@
-var getData=function(){
-    var nombreI=document.getElementById("nombre").value;
-    var apellidoI=document.getElementById("apellido").value;
-    var cedulaI=document.getElementById("cedula").value;
-    var correoI=document.getElementById("correo").value;
-    var contrasenaI=document.getElementById("contrasena").value;
-    if(nombreI=="" || apellidoI=="" || cedulaI=="" || correoI=="" || contrasenaI ==""){
+var getData = function () {
+    var nombreI = document.getElementById("nombre").value;
+    var apellidoI = document.getElementById("apellido").value;
+    var cedulaI = document.getElementById("cedula").value;
+    var correoI = document.getElementById("correo").value;
+    var contrasenaI = document.getElementById("contrasena").value;
+    console.log(nombreI);
+    console.log(apellidoI);
+    console.log(cedulaI);
+    console.log(correoI);
+    console.log(contrasenaI);
+    if (nombreI == "" || apellidoI == "" || cedulaI == "" || correoI == "" || contrasenaI == "") {
         alert("Diligencie Completamente Los Datos");
-    }if(isNaN(cedulaI)){
+    } if (isNaN(cedulaI)) {
         alert("Dijite Datos Validos");
-    }else{
-        axios.post('ec2-3-81-53-23.compute-1.amazonaws.com:8086/cats/usuario', {
-            cedula: cedulaI,
-            nombre: nombreI,
-            apellido: apellidoI,
-            contrasena: contrasenaI,
-            Correo: correoI
-
-        })
-        .then(function(response) {
-            console.log(response + ' Se hizo post y se añadio el usuario a la base de datos')
-            window.location.assign('login.html')
-        })
-        .catch(function(error) {
-            console.log(error + ' No se logro hacer post')
-        })
-        
     }
 
+    else {
+        axios.post('http://ec2-54-204-59-190.compute-1.amazonaws.com:8086/cats/usuario', {
+            nombre: nombreI,
+            apellido: apellidoI,
+            cedula: cedulaI,
+            contrasena: contrasenaI,
+            correo: correoI
 
- }
-    
+        })
+            .then(function (response) {
+                console.log(response + ' Se hizo post y se añadio el usuario a la base de datos')
+                window.location.assign('login.html')
+            })
+            .catch(function (error) {
+                console.log(error + ' No se logro hacer post')
+            })
+
+    }
+}
+
+var getUser = function () {
+    var nombreU = $("#correo").val();
+    var passwordU = $("#correo").val();
+    axios.get('http://ec2-54-204-59-190.compute-1.amazonaws.com:8086/cats/correo' + usuario)
+        .then(function (response) {
+            var articulosEnCarrito
+            passwordUser = response.data.contrasena;
+            $('#contadorCarrito').attr('data-count', articulosEnCarrito)
+        })
+
+
+
+
+
+
+
+
+
+}
+
